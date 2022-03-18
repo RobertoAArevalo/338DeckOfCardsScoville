3f05d96b45467e6c2884ff2d7fff146233ad6ea9
ddelaCSUMB <101760560+ddelaCSUMB@users.noreply.github.com>
2022-03-17 22:56:15 -0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,6 +1,36 @@
 public class main {
    ///ali test
    ///ali test 2
-
+   //wuddup -dave
    /// this is what is up
+   //Richard is here
+   
+   /*
+   enum Suit{
+    HEARTS,
+    DIAMONDS,
+    CLUBS,
+    SPADES,   
+    }
+   
+   private char value;
+   private Suit suit;
+   private boolean cardError;
+   
+   public Card (char value, Suit suit)
+   {
+      this.value = value;
+      this.suit = suit;
+   
+   }
+   
+   public Card( )
+   {
+      value = 
+   }
+   
+   
+   */
+   
+   System.out.println("hello world");
 }

1e8079bf782d95e805aa03903b0d9e0c38dd7653
ddelaCSUMB <101760560+ddelaCSUMB@users.noreply.github.com>
2022-03-17 22:33:31 -0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -4,5 +4,21 @@ public class main {
    //wuddup -dave
    /// this is what is up
    //Richard is here
+   
+   /*
+   enum Suit{
+    HEARTS,
+    DIAMONDS,
+    CLUBS,
+    SPADES,   
+    }
+   
+   private char value;
+   private Suit suit;
+   private boolean cardError;
+   
+   
+   */
+   
    System.out.println("hello world");
 }

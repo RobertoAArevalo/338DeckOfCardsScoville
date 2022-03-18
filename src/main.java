c7b91da1b5b28d4284999f5c5f8eb4c565b8c0f6
Richard Garcia <97427644+ricgarciacsumb@users.noreply.github.com>
2022-03-18 00:15:41 -0700

    printed eclipse sucks and hello world

--- src/main.java
+++ src/main.java
@@ -1,36 +1,50 @@
-public class main {
-   ///ali test
-   ///ali test 2
-   //wuddup -dave
-   /// this is what is up
-   //Richard is here
-   
+public class main
+{
+    public static void main(String[] args)
+    {
+        ///ali test 2
+        //wuddup -dave
+        /// this is what is up
+        //Richard is here
+
    /*
+
+
    enum Suit{
     HEARTS,
     DIAMONDS,
     CLUBS,
-    SPADES,   
+    SPADES,
     }
-   
+
    private char value;
    private Suit suit;
    private boolean cardError;
-   
+
    public Card (char value, Suit suit)
    {
       this.value = value;
       this.suit = suit;
-   
+
    }
-   
+
    public Card( )
    {
       value = Card.SPADES;
    }
-   
-   
-   */
-   
+
+    */
+
+
+
+   System.out.println("Eclipse sucks");
    System.out.println("hello world");
+
+    }
+
 }
+
+
+   ///ali test
+
+

40bb32282542ce13a2c5da338ced5cbb58fe40ba
Ali Bakr <abakr@csumb.edu>
2022-03-20 14:07:45 -0700

    Add shell to class.  Opening code with me

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,45 @@
 public class Deck
 {
+   public final int MAX_CARDS = 312;
+   private static main.Card[] masterPack = new main.Card[52];
+   private main.Card[] cards;
+   private int topCard;
 
+   Deck(int numPacks)
+   {
+      if (numPacks == 0)
+      {
+         numPacks = 1;
+      }
+   }
+
+   public void init(int numPacks)
+   {
+
+   }
+
+   public void shuffle()
+   {
+
+   }
+
+   public main.Card dealCard()
+   {
+
+   }
+
+   public int getTopCard()
+   {
+      return topCard;
+   }
+
+   public main.Card inspectCard(int k)
+   {
+
+   }
+
+   private static void allocateMasterPack()
+   {
+
+   }
 }

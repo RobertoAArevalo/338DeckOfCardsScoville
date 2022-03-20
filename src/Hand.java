c93e5d9a0f161d892db2141e7becb384ddd7d9ab
ddelaCSUMB <101760560+ddelaCSUMB@users.noreply.github.com>
2022-03-20 01:10:46 -0700

    Update Hand.java

--- src/Hand.java
+++ src/Hand.java
@@ -1,21 +1,25 @@
 public class Hand {
-  
+      
+      //series of variables used
       public final int MAX_CARDS = 75;
       private Card[] myCards;
       private int numCards;
       
+      //initializes integer numCards and array myCards
       public Hand()
       {
          numCards = 0;
          myCards = new Card[MAX_CARDS];
       }
       
+      //resets numCards and myCards by overwiting with initial values
       public void resetHand()
       {
          numCards = 0;
          myCards = new Card[MAX_CARDS];
       }
       
+      //adds a card to the array myCards
       public boolean takeCard(Card card)
       {
          if(numCards >= MAX_CARDS)

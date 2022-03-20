b7df3e99c3ac7fc667d9b3ee45c8f3d8acb7cc43
ddelaCSUMB <101760560+ddelaCSUMB@users.noreply.github.com>
2022-03-20 01:05:49 -0700

    Update Hand.java

--- src/Hand.java
+++ src/Hand.java
@@ -1,2 +1,33 @@
 public class Hand {
+  
+      public final int MAX_CARDS = 75;
+      private Card[] myCards;
+      private int numCards;
+      
+      public Hand()
+      {
+         numCards = 0;
+         myCards = new Card[MAX_CARDS];
+      }
+      
+      public void resetHand()
+      {
+         numCards = 0;
+         myCards = new Card[MAX_CARDS];
+      }
+      
+      public boolean takeCard(Card card)
+      {
+         if(numCards >= MAX_CARDS)
+         {
+            System.out.println("Error: Adding to a full array.");
+            return false;
+         }
+         else
+         {
+            myCards[numCards] = card;
+            numCards++;
+            return true;
+         }
+      }
 }

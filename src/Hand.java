7e9d3e0528bfe524a54ee8a56824cf16e977c6f8
ddelaCSUMB <101760560+ddelaCSUMB@users.noreply.github.com>
2022-03-20 20:09:49 -0700

    Update Hand.java

--- src/Hand.java
+++ src/Hand.java
@@ -62,9 +62,8 @@ public class Hand {
       
       
       //this method returns last card of array or returns bad card error
-            public Card playCard()
-      {
-         
+      public Card playCard()
+      {     
          if(numCards == 0)
          {
             return System.out.println(""Bad Card" error");
